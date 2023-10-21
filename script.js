06843897ccc28e38442e75bdd2815714adf79aa3
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-10-22 01:44:59 +0530

    first for this project

---
+++ script.js
@@ -0,0 +1,44 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+var tl = gsap.timeline()
+
+tl.to("#main",{
+    y:"100vh",
+    scale:1,
+    duration:0
+})
+tl.to("#main",{
+    y:"30vh",
+    duration:1,
+    delay:1
+})
+tl.to("#main",{
+    y:"0vh",
+    rotate:720,
+    scale:1,
+    duration:0.7
+})
+
+
+
+/*page2*/
+// tl.to("#page2",{
+//     y:"100vh",
+//     scale:0.6,
+//     duration:0
+// })
+// tl.to("#page2",{
+//     y:"30vh",
+//     duration:1,
+//     delay:1
+// })
+// tl.to("#page2",{
+//     y:"0vh",
+//     rotate:1460,
+//     scale:1,
+//     duration:0.7
+// })
