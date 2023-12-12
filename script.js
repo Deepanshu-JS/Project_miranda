a49c26fe5389078526801ba92864d54c63f340f1
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-12-12 23:50:34 +0530

    day 32

--- script.js
+++ script.js
@@ -26,19 +26,19 @@ tl.to("#main",{
 
 
 /*page2*/
-// tl.to("#page2",{
-//     y:"100vh",
-//     scale:0.6,
-//     duration:0
-// })
-// tl.to("#page2",{
-//     y:"30vh",
-//     duration:1,
-//     delay:1
-// })
-// tl.to("#page2",{
-//     y:"0vh",
-//     rotate:1460,
-//     scale:1,
-//     duration:0.7
-// })
+tl.to("#page2",{
+    y:"100vh",
+    scale:0.6,
+    duration:0
+})
+tl.to("#page2",{
+    y:"30vh",
+    duration:1,
+    delay:1
+})
+tl.to("#page2",{
+    y:"0vh",
+    rotate:1460,
+    scale:1,
+    duration:0.7
+})
