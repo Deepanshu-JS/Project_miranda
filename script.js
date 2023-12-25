bd02766b1b9c2893be132221a2a080cf0b328f43
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-12-26 01:10:31 +0530

    full responsive day 36

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ tl.to("#main",{
 tl.to("#main",{
     y:"30vh",
     duration:1,
-    delay:1
+    delay:1,
 })
 tl.to("#main",{
     y:"0vh",
@@ -25,20 +25,22 @@ tl.to("#main",{
 
 
 
-/*page2*/
-tl.to("#page2",{
-    y:"100vh",
-    scale:0.6,
-    duration:0
-})
-tl.to("#page2",{
-    y:"30vh",
-    duration:1,
-    delay:1
-})
-tl.to("#page2",{
-    y:"0vh",
-    rotate:1460,
-    scale:1,
-    duration:0.7
-})
+
+
+// /*page2*/
+// tl.to("#page2",{
+//     y:"100vh",
+//     scale:0.6,
+//     duration:0
+// })
+// tl.to("#page2",{
+//     y:"30vh",
+//     duration:1,
+//     delay:1
+// })
+// tl.to("#page2",{
+//     y:"0vh",
+//     rotate:1460,
+//     scale:1,
+//     duration:0.7
+// })
