12ea05e1e0d361d0ed1e41419925ac7fbd07089f
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-12-26 14:21:27 +0530

    day 35

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const scroll = new LocomotiveScroll({
 
 var tl = gsap.timeline()
 
+tl.to("#main", {
+    delay:6
+  })
+
+
 tl.to("#main",{
     y:"100vh",
     scale:1,
@@ -15,6 +20,7 @@ tl.to("#main",{
     y:"30vh",
     duration:1,
     delay:1,
+    
 })
 tl.to("#main",{
     y:"0vh",
@@ -26,21 +32,43 @@ tl.to("#main",{
 
 
 
+const tl2 = gsap.timeline();
 
-// /*page2*/
-// tl.to("#page2",{
-//     y:"100vh",
-//     scale:0.6,
-//     duration:0
-// })
-// tl.to("#page2",{
-//     y:"30vh",
-//     duration:1,
-//     delay:1
-// })
-// tl.to("#page2",{
-//     y:"0vh",
-//     rotate:1460,
-//     scale:1,
-//     duration:0.7
-// })
+tl2.to("body", {
+  overflow: "hidden"
+})
+  .to(".preloader .text-container", {
+    duration: 0,
+    opacity: 1,
+    ease: "Power3.easeOut"
+  })
+  .from(".preloader .text-container h1", {
+    duration: 2.5,
+    delay: 1,
+    y: 70,
+    skewY: 10,
+    stagger: 0.4,
+    ease: "Power3.easeOut"
+  })
+  .to(".preloader .text-container h1", {
+    duration: 2.2,
+    y: 70,
+    skewY: -20,
+    stagger: 0.2,
+    ease: "Power3.easeOut"
+  })
+  .to(".preloader", {
+    duration: 1.5,
+    height: "0vh",
+    ease: "Power3.easeOut"
+  })
+  .to(
+    "body",
+    {
+      overflow: "hidden"
+    },
+    "-=2"
+  )
+  .to(".preloader", {
+    display: "none"
+  });
